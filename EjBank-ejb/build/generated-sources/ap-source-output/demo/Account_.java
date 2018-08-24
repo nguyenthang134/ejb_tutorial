7a14aa69c91fc4b7751537b7eb8065fc370e620d
thangnal <thangna@nal.vn>
2018-08-24 22:51:09 +0700

    abc

--- EjBank-ejb/build/generated-sources/ap-source-output/demo/Account_.java
+++ EjBank-ejb/build/generated-sources/ap-source-output/demo/Account_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-17T20:35:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-17T20:54:04")
 @StaticMetamodel(Account.class)
 public class Account_ { 
 
